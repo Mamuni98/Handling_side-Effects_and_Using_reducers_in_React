5cca7ad5572b9b563eada0d280a69795fc34566d
Mamuni98 <nabanitasahoo468@gmail.com>
2023-04-05 11:44:44 +0530

    Debouncing_in_useEffect

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,21 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Card from '../UI/Card/Card';
-import classes from './Login.module.css';
-import Button from '../UI/Button/Button';
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
 
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState("");
   const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState("");
   const [passwordIsValid, setPasswordIsValid] = useState();
-  const [enteredCollege, setEnteredCollege] = useState('');
+  const [enteredCollege, setEnteredCollege] = useState("");
   const [collegeIsValid, setCollegeIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-  useEffect(() => {setFormIsValid(
-    enteredEmail.includes('@') && enteredPassword.trim().length > 6 && enteredCollege.trim().length > 0
-  );}, [enteredEmail, enteredPassword, enteredCollege])
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      console.log("Effect running");
+      setFormIsValid(
+        enteredEmail.includes("@") &&
+          enteredPassword.trim().length > 6 &&
+          enteredCollege.trim().length > 0
+      );
+    }, 1000);
+    
+    return () => {
+      console.log("Effect cleanup");
+      clearTimeout(identifier);
+    };
+  }, [enteredEmail, enteredPassword, enteredCollege]);
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
@@ -30,7 +42,7 @@ const Login = (props) => {
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    setEmailIsValid(enteredEmail.includes("@"));
   };
 
   const validatePasswordHandler = () => {
@@ -40,7 +52,6 @@ const Login = (props) => {
   const validateCollegeHandler = () => {
     setCollegeIsValid(enteredCollege.trim().length > 0);
   };
-
 
   const submitHandler = (event) => {
     event.preventDefault();
@@ -52,7 +63,7 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailIsValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="email">E-Mail</label>
@@ -66,7 +77,7 @@ const Login = (props) => {
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
+            passwordIsValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
@@ -80,7 +91,7 @@ const Login = (props) => {
         </div>
         <div
           className={`${classes.control} ${
-            collegeIsValid === false ? classes.invalid : ''
+            collegeIsValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="college">College Name</label>
